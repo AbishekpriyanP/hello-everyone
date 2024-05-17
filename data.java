cfb15cf0fb385700809e75478262a6da56cb903c
AbishekpriyanP <r.p.abishekpriyan3672@gmail.com>
2024-05-17 13:01:29 +0530

    new branch

--- data.java
+++ data.java
@@ -1,8 +1,5 @@
 public statis void main(String[] args){
-<<<<<<< HEAD
     System.out.println("New pull Changes");
     System.out.println("New pull Local");
-=======
     System.out.println("Old pull Changes");
->>>>>>> origin/submain
 }
